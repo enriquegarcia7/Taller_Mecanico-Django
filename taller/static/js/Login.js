eabc7037b55294e759a75895ba74587221c467f2
enriquegarcia7 <enrique7729@gmail.com>
2023-06-16 12:19:04 -0400

    Projecto migrado a Django

---
+++ taller/static/js/Login.js
@@ -0,0 +1,42 @@
+document.getElementById("login-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var userType = "";
+
+  if (email === "" || password === "") {
+    var modal = document.getElementById("modal1");
+    modal.showModal();   
+    return;
+  }
+
+  var sanitizedEmail = email.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  var sanitizedPassword = password.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+  const users = ["mecanico@rmakween.cl", "admin@rmakween.cl"];
+  const pass = ["mecanico", "admin"];
+
+  if (sanitizedEmail === users[0] && sanitizedPassword === pass[0]) {
+    userType = "mecanico";
+  } else if (sanitizedEmail === users[1] && sanitizedPassword === pass[1]) {
+    userType = "administrador";
+  } else {
+    var modal = document.getElementById("modal2");
+    modal.showModal();
+   
+    return;
+  }
+
+
+  if (userType === "mecanico") {
+    window.location.href = "Mecanico.html";
+  } else if (userType === "administrador") {
+    window.location.href = "Admin.html";
+  }
+});
+function cerrar() {
+  var modal = document.getElementById("modal1");
+  modal.close();
+  var modal = document.getElementById("modal2");
+  modal.close();
+}
