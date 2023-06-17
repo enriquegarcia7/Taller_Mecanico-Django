4f77387977f83c14f391e290269dd7fd9a93a464
enriquegarcia7 <enrique7729@gmail.com>
2023-06-17 09:43:40 -0400

    index correguido

--- taller/views.py
+++ taller/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'taller/index.html', {})
+    return render(request, 'taller/Index.html', {})
