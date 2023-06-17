4d98f99def68d3093066af78a456f24f442b530e
acevedo86 <dani.acevedo@duocuc.com>
2023-06-17 15:14:39 -0400

    3 views agregadas Agendar, index, login

--- taller/views.py
+++ taller/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'taller/Index.html', {})
+
+def login(request):
+    return render(request, 'taller/Login.html', {})
+
+def agendar(request):
+    return render(request, 'taller/Agendar.html', {})
