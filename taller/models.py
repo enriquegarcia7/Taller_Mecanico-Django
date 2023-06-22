b2d5148a91bfec414d1513e1d118f1dc10ae1188
enriquegarcia7 <enrique7729@gmail.com>
2023-06-22 09:06:55 -0400

    base de datos arreglada

--- taller/models.py
+++ taller/models.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 class Reserva(models.Model):
-    Rut = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    Rut = models.CharField(max_length=50)
     Nombre = models.CharField(max_length=50)
     Telefono = models.IntegerField()
     Email = models.CharField(max_length=50)
@@ -11,10 +13,11 @@ class Reserva(models.Model):
     Fecha_Reserva = models.DateTimeField(blank=True, null=True)
 
     def clean(self):
-        if self.fecha_reserva < timezone.now().date():
+        if self.Fecha_Reserva.date() < timezone.now().date():
             raise ValidationError('La fecha de reserva debe ser posterior a la fecha actual.')
 
-
+    def __str__(self):
+        return f"Reserva: {self.Nombre} ({self.Fecha_Reserva})"
 
 #python manage.py makemigrations
 #python manage.py migrate
