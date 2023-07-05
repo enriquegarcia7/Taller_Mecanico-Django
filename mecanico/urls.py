8a6d3a05c0f5145ed19ee3fa34835a2e02c425c1
acevedo86 <dani.acevedo@duocuc.com>
2023-07-04 23:30:32 -0400

    login2.0

--- mecanico/urls.py
+++ mecanico/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('taller.urls'))
+    path('', include('taller.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 if settings.DEBUG:
